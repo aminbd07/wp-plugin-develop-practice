3789144900452e230bd3a9b0f6c5b510cd48da75
Nurul Amin <amin.ict@gmail.com>
2023-08-17 00:59:42 +0600

    Play with WP AJAX and wpdb

--- include/welcome.php
+++ include/welcome.php
@@ -1,2 +1,63 @@
+<?php
+// if ($_POST) {
+//     echo "<h3>Post Data</h3>";
+//     var_dump($_POST);
+// }
+global $wpdb;
+$table_name = $wpdb->prefix . 'student_list';
+$students = $wpdb->get_results("SELECT * FROM {$table_name} ");
+?>
+
+
+
 <h2>Welcome </h2>
 <p>Welcome to our plugin home </p>
+<div>
+    All Students :
+    <div class='itemRow'>
+        <?php
+        foreach ($students as $student) {
+            echo "<p > ID : {$student->id} :  {$student->name}</p>";
+        }
+        ?>
+    </div>
+</div>
+<form id="myForm">
+    <input type="hidden" name="action" value="mfp-save-my-data" />
+    <label>Name
+        <input type="text" name="name" value="" id="name" />
+    </label>
+
+    <label>Class
+        <input type="text" name="class" value="" id="class" />
+    </label>
+
+    <label>Age
+        <input type="text" name="age" value="" id="age" />
+    </label>
+
+    <input type="submit" name="submit" value="Save" />
+
+</form>
+
+<script>
+
+    jQuery("#myForm").submit(function(e) {
+        e.preventDefault();
+        let data = {
+            action: 'mfp-save-my-data',
+            name: jQuery("#name").val(),
+            class: jQuery("#class").val(),
+            age: jQuery("#age").val(),
+        };
+
+        jQuery.post(mfpAjaxVar.ajaxurl, data,
+            function(response) {
+                if (response.success) {
+                    let html = "<p> ID : " + response.id + "  : " + jQuery("#name").val() + "   </p>";
+                    jQuery(".itemRow").append(html)
+                }
+            }
+        );
+    })
+</script>
--- myfirstpluin.php
+++ myfirstpluin.php
@@ -18,6 +18,24 @@
 
 function callOnPluginActivation()
 {
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'student_list'; // Replace with your table name
+
+    // Check if the table exists
+    if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
+        // Table doesn't exist, create it
+        $sql = "CREATE TABLE $table_name (
+            id INT NOT NULL AUTO_INCREMENT,
+            name VARCHAR(255) NOT NULL,
+            class VARCHAR(255) NOT NULL,
+            age INTEGER(3),
+            PRIMARY KEY (id)
+        ) $wpdb->charset_collate;";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
 }
 
 register_activation_hook(
@@ -27,20 +45,36 @@ register_activation_hook(
 
 
 
+add_action('admin_init', 'loadDefineData');
 add_action('admin_init', 'loadAdminSetings');
 
 
 function loadAdminSetings()
 {
     register_setting('general', 'Site_Country');
-
     add_settings_field('field_id-site-country', "Site Country", 'showSettingFileds', 'general', 'default', array(
         'id' => 'field_id-site-country_',
         'option_name' => 'Site_Country'
     ));
+    wp_enqueue_script('mfp-frontend-ajax', JS_DIR_URI . 'script.js', array('jquery'), null, false);
+    wp_localize_script(
+        'mfp-frontend-ajax',
+        'mfpAjaxVar',
+        array(
+            'ajaxurl' => admin_url('admin-ajax.php'),
+            'author' => 'Nurul Amin',
+            'serverTime' => date('Y-m-d h:i:s'),
+        )
+    );
+
+
+    add_action('wp_ajax_mfp-save-my-data', 'handleMyAjaxData');
 }
 
-
+function loadDefineData()
+{
+    define('JS_DIR_URI', __DIR__ . "/js/");
+}
 
 
 /**
@@ -104,11 +138,20 @@ function mfp_register_meu()
         __('My First Plugin', 'mfp'),
         __('Plugin Home', 'mfp'),
         'manage_options',
-        'admin.php',
+        'myfirstplugin.php',
         'loadMyPluginHome',
         // plugins_url('myplugin/images/icon.png'),
         'dashicons-admin-site-alt2',
         6
+    );
+
+    add_submenu_page(
+        'myfirstplugin.php',
+        __('Sub Menu', 'mfp'),
+        __('My Sub Menu', 'mfp'),
+        'manage_options',
+        'my-sub-menu',
+        'loadSubmenu'
     );
 }
 add_action('admin_menu', 'mfp_register_meu');
@@ -117,4 +160,34 @@ add_action('admin_menu', 'mfp_register_meu');
 function loadMyPluginHome()
 {
     include('include/welcome.php');
+}
+
+
+function loadSubmenu()
+{
+?>
+    Sub Menu page 
+<?php
+}
+
+
+function handleMyAjaxData()
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'student_list';
+    $success =  $wpdb->insert(
+        $table_name,
+        array(
+            'name' => $_POST['name'],
+            'class' => $_POST['class'],
+            'age' => $_POST['age'],
+        ),
+        array(
+            '%s',
+            '%s',
+            '%d',
+        )
+    );
+    wp_send_json(['success' => true, 'id' => $wpdb->insert_id]);
+    wp_die();
 }
